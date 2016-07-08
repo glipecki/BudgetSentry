c19ce2f5082fa887863eaa81a25cf047226a6319
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2016-07-08 13:48:12 +0200

    project bootstrap

---
+++ src/main/java/net/liepcki/budgetsentry/BudgetSentryApplication.java
@@ -0,0 +1,13 @@
+package net.liepcki.budgetsentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSentryApplication.class, args);
+	}
+
+}
